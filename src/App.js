fd9cf794c9d33e6a8da9b115326257f574ab7950
Walter O. Achammer <info@blackboxmd.fyi>
2019-06-17 21:58:41 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import List from './Components/List'
+import Title from './Components/Title'
+
+
+class App extends Component {
+  constructor() {
+      super()
+      this.state = {
+          names: ['Johnny','Smith', 'Harry']
+      }
+      this.removeName = this.removeName.bind(this)
+  }
+  removeName(nameRemoved) {
+      console.log(nameRemoved)
+      this.setState((state) => ({
+        names: state.names.filter( name => name !== nameRemoved)
+      })   
+      )
+  }
+  render() {
+      return(
+          <div>
+              <Title title = {'Studentenliste'} />
+              <List students = {this.state.names} onRemoveName = {this.removeName}  />
+          </div>
+      );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+// import { jsxNamespacedName } from '@babel/types';
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
