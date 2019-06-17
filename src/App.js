79f7d10c98a0b19ad8bc36968c1a038cd4d066c7
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-06-17 21:53:54 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import List from './Components/List'
+import Title from './Components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+      super()
+      this.state = {
+          names: ['Johnny','Smith', 'Harry']
+      }
+      this.removeName = this.removeName.bind(this)
+  }
+  removeName(nameRemoved) {
+      console.log(nameRemoved)
+      this.setState((state) => ({
+        names: state.names.filter( name => name !== nameRemoved)
+      })   
+      )
+  }
+  render() {
+      return(
+          <div>
+              <Title title = {'Studentenliste'} />
+              <List students = {this.state.names} onRemoveName = {this.removeName}  />
+          </div>
+      );
+  }
 }
 
 export default App;
