1e5b822118621f0e98f2239b5d313196ade62c94
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-06-18 22:03:34 +0200

    Before routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Title from './Components/Title'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Services from './Pages/Services'
+import Blogs from './Pages/Blogs'
+import Contact from './Pages/Contact'
+
 class App extends Component {
   constructor() {
       super()
@@ -33,6 +39,11 @@ class App extends Component {
       return(
           <div className = "">
               <Navbar />
+              <Home />
+              <About />
+              <Services />
+              <Blogs />
+              <Contact />              
               <Title title = {'Studentenliste'} />
               <List students = {this.state.names} onRemoveName = {this.removeName}  />
               <Footer />
