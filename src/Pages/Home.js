368fa137e045256ed54dbaa15e9f8a3781aa46e5
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-06-20 11:58:35 +0200

    HP part one

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -30,15 +30,15 @@ class Home extends Component {
                 </div>
                 {/* Carousel section ends here */}
                 <section id="feature">
-                    <div className="container">
+                    <div className="container text-center pt-4">
                         <h2>Features</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum vitae ex tempus sagittis vel eu magna. Quisque nunc nulla, sollicitudin in tincidunt eu, condimentum at ante.</p>
+                        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum vitae ex tempus sagittis vel eu magna. Quisque nunc nulla, sollicitudin in tincidunt eu, condimentum at ante.</p>
                     </div>
                     <div className="Container">
                         <div className="row">
                             <div className="col-md-4">
                                 <div ClassName="feature-wrap">
-                                    <i className="fa fa-laptop">
+                                    <i className="fas fa-laptop">
                                         <h2>Fresh and Clean</h2>
                                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                                     </i>
@@ -46,35 +46,35 @@ class Home extends Component {
                             </div>
                             <div className="col-md-4">
                                 <div ClassName="feature-wrap">
-                                    <i className="fa fa-laptop">
+                                    <i className="fas fa-comments">
                                         <h2>Fresh and Clean</h2>
                                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                                     </i>
                                 </div>
                             </div><div className="col-md-4">
                                 <div ClassName="feature-wrap">
-                                    <i className="fa fa-laptop">
+                                    <i className="fas fa-leaf">
                                         <h2>Fresh and Clean</h2>
                                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                                     </i>
                                 </div>
                             </div><div className="col-md-4">
                                 <div ClassName="feature-wrap">
-                                    <i className="fa fa-laptop">
+                                    <i className="fas fa-cogs">
                                         <h2>Fresh and Clean</h2>
                                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                                     </i>
                                 </div>
                             </div><div className="col-md-4">
                                 <div ClassName="feature-wrap">
-                                    <i className="fa fa-laptop">
+                                    <i className="fas fa-download">
                                         <h2>Fresh and Clean</h2>
                                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                                     </i>
                                 </div>
                             </div><div className="col-md-4">
                                 <div ClassName="feature-wrap">
-                                    <i className="fa fa-laptop">
+                                    <i className="fas fa-heart">
                                         <h2>Fresh and Clean</h2>
                                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                                     </i>
