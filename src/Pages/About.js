864fa70d47d5c5a225eae9a972180d8431755e84
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-06-22 19:53:58 +0200

    Contentful eingebaut

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,16 +1,34 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import client from './Client'
+import BlackLoader from '../images/black-loader.gif'
 
 class About extends Component {
+    constructor() {
+        super()
+        this.state = {aboutPage: []}
+    }
+    componentDidMount() {
+        client.getEntries({
+            'content_type' : 'about'
+        }).then((entries) => {
+            this.setState({aboutpage: entries.items[0]}) // 200
+        })
+    }
+
     render() {
-        return(
+        return (
             <div>
                 <div className="container">
-                <h2 className="text-center">About Us</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc euismod nisi sit amet est consectetur, sit amet consequat nibh tempor. Vivamus ac purus elit. Mauris tincidunt dignissim felis sed bibendum. Curabitur euismod hendrerit sagittis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras ultricies sagittis nibh. Ut luctus sagittis lectus, non convallis tellus maximus a. Cras interdum nisi et auctor imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi eget eros blandit, consectetur ex vel, sollicitudin lectus.</p>
+                    <h2 className="text-center">About Us</h2>
+                    {this.state.aboutPage.length === 0 ?
+                        <div align="center" className="pt-5"> <img src={BlackLoader} alt="Loader" /></div>
+                        :
+                        <p>{this.state.aboutpage.fields.aboutDescription}</p>
+                    }
                 </div>
             </div>
-        );
-    }
-}
-
+                );
+            }
+        }
+        
 export default About
