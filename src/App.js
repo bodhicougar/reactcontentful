9593e0df48ceb57e2cf8feb3963cddcf88384be2
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-06-24 16:56:16 +0200

    App.js angepasst

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './Components/Footer'
 
 import Home from './Pages/Home'
 import About from './Pages/About'
-import Services from './Pages/Services_altfalsch'
+import Services from './Pages/Services'
 import Blogs from './Pages/Blogs'
 import Contact from './Pages/Contact'
 
