a63c3cc25ce84082cf9ce583aa855bf48e494c7b
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-06-27 18:02:09 +0200

    Finito. Allerdings nicht ganz sauber.

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import client from './Client'
 import BlackLoader from '../images/black-loader.gif'
 import marked from 'marked'
@@ -6,31 +6,32 @@ import marked from 'marked'
 class About extends Component {
     constructor() {
         super()
-        this.state = { aboutPage: []}
+        this.state = {aboutpage: []}
     }
+
     componentDidMount() {
         client.getEntries({
-            'content_type': 'about'
-        }).then((entries) => {
-            this.setState({ aboutpage: entries.items[0] })
-        })
+            'content_type' : 'about'
+          }).then( (entries) => {
+            this.setState({aboutpage: entries.items[0]}) 
+          })
     }
 
     getParsedMarkdown(aboutDescription) {
-        return {
-            __html: marked(aboutDescription, { sanitize: true })
+        return{
+            __html:marked(aboutDescription, {sanitize:true})
         }
     }
 
     render() {
-        return (
+        return(
             <div>
                 <div className="container">
-                    <h2 className="text-center">About Us</h2>
-                    {this.state.aboutPage.length === 0 ?
-                        <div align="center" className="pt-5"> <img src={BlackLoader} alt="Loader" /></div>
+                    <h2 className="text-center">Was wir so machen...</h2>
+                    {this.state.aboutpage.length === 0 ?
+                        <div align="center" className="pt-5"> <img src={BlackLoader} alt="Loader" /> </div>
                         :
-                        <div dangerouslySetInnerHTML={this.getParsedMarkdown(this.state.aboutpage.fields.aboutDescription)}>
+                        <div dangerouslySetInnerHTML = {this.getParsedMarkdown(this.state.aboutpage.fields.aboutDescription)}>
 
                         </div>
                         // <p>{this.state.aboutpage.fields.aboutDescription}</p>
@@ -40,5 +41,4 @@ class About extends Component {
         );
     }
 }
-
 export default About
